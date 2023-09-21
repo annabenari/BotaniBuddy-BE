f22f4ab4aedf37d1e8eb7f7b00ba91cf67ede7f1
chrisRduckworth <c_duckworth@hotmail.com>
2023-09-21 14:05:54 +0100

    add 201 test for POST /api/users/user_id/add_by_search

--- __tests__/integration.tests.js
+++ __tests__/integration.tests.js
@@ -4,6 +4,7 @@ const seed = require("../db/seeds/seed");
 const request = require("supertest");
 const mongoose = require("mongoose");
 const testData = require("../db/data/test-data/index");
+const { usersSchema } = require("../db/seeds/models");
 
 beforeEach(async () => {
   await seed(testData);
@@ -86,75 +87,90 @@ describe("POST /api/login allows a user to login on the app", () => {
           .expect(200);
       })
       .then(({ body }) => {
-
-       
-
         expect(body.user.msg).toBe("Login succesful");
         expect(body.user.username).toBe("Chris");
-        expect(body.user).toHaveProperty("user_id", expect.any((String)));
+        expect(body.user).toHaveProperty("user_id", expect.any(String));
       });
   });
 
-  test("Status 400: responds with error when given incorrect password", ()=>{
+  test("Status 400: responds with error when given incorrect password", () => {
     return request(app)
-    .post("/api/register")
-    .send({
-      username: "Chris",
-      password: "bananas",
-    })
-    .then(() => {
-      return request(app)
-        .post("/api/login")
-        .send({
-          username: "Chris",
-          password: "HELLO",
-        })
-        .expect(400);
-    })
-    .then(({body})=>{
-        
-        expect(body.msg).toBe("Bad request")
-        expect(body.detail).toBe("Password does not match")
-    })
-  })
-  test("Status 404: no user found", ()=>{
-    return request(app)
-        .post("/api/login")
-        .send({
-          username: "UNKNOWNUSER",
-          password: "HELLO",
-        })
-        .expect(404)
-        .then(({body})=>{
-            expect(body.msg).toBe("Not Found")
-            expect(body.detail).toBe("No user found with this username")
-        })
-  })  
-
-  test("Status 400: bad request - invalid input - no username", ()=>{
-    return request(app)
-        .post("/api/login")
-        .send({
-          password: "HELLO",
-        })
-        .expect(400)
-        .then(({body})=>{
-            expect(body.msg).toBe("Bad Request")
-            expect(body.detail).toBe("Invalid Input")
-        })
-  })  
-  test("Status 400: bad request - invalid input - no password", ()=>{
-    return request(app)
-        .post("/api/login")
-        .send({
+      .post("/api/register")
+      .send({
         username: "Chris",
-        })
-        .expect(400)
-        .then(({body})=>{
-            expect(body.msg).toBe("Bad Request")
-            expect(body.detail).toBe("Invalid Input")
-        })
-  })  
+        password: "bananas",
+      })
+      .then(() => {
+        return request(app)
+          .post("/api/login")
+          .send({
+            username: "Chris",
+            password: "HELLO",
+          })
+          .expect(400);
+      })
+      .then(({ body }) => {
+        expect(body.msg).toBe("Bad request");
+        expect(body.detail).toBe("Password does not match");
+      });
+  });
+  test("Status 404: no user found", () => {
+    return request(app)
+      .post("/api/login")
+      .send({
+        username: "UNKNOWNUSER",
+        password: "HELLO",
+      })
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Not Found");
+        expect(body.detail).toBe("No user found with this username");
+      });
+  });
 
-  
+  test("Status 400: bad request - invalid input - no username", () => {
+    return request(app)
+      .post("/api/login")
+      .send({
+        password: "HELLO",
+      })
+      .expect(400)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Bad Request");
+        expect(body.detail).toBe("Invalid Input");
+      });
+  });
+  test("Status 400: bad request - invalid input - no password", () => {
+    return request(app)
+      .post("/api/login")
+      .send({
+        username: "Chris",
+      })
+      .expect(400)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Bad Request");
+        expect(body.detail).toBe("Invalid Input");
+      });
+  });
+});
+
+describe.only("POST /api/users/:user_id/add_by_search allows a user to add a plant they have searched for", () => {
+  test("Status 201: creates a new plant in user's plants", () => {
+    const Users = mongoose.model("users", usersSchema);
+    return Users.find({}, null, { limit: 1 }).then(([user]) => {
+      return request(app)
+        .post(`/api/users/${user._id}/add_by_search`)
+        .send({ name: "monstera" })
+        .expect(201)
+        .then(({ body: { plant } }) => {
+          expect(plant).toHaveProperty("_id", expect.any(String));
+          expect(plant).toHaveProperty("tasks", expect.any(Object));
+          expect(plant).toHaveProperty(
+            "users",
+            expect.arrayContaining([user._id])
+          );
+          expect(plant).toHaveProperty("plantType", expect.any(String));
+        });
+    });
+  });
 });
