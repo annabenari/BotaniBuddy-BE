b3185c3ba84e4e62d77aa1c7e5332cdf339c2483
chrisRduckworth <c_duckworth@hotmail.com>
2023-09-22 15:50:31 +0100

    test NODE_ENV

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,6 @@
 const mongoose = require("mongoose")
 
-const ENV = process.env.URL || "development"
-
-console.log(ENV, "< ENV")
+const ENV = process.env.NODE_ENV || "development"
 
 require("dotenv").config({path: `${__dirname}/../.env.${ENV}`})
 
