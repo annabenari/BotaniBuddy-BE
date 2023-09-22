f99423b9599f48f0d85619f3742b0464f4d2478f
annabenari <annabenari@me.com>
2023-09-22 12:23:22 +0100

    attempting to host part 2 usuing cyclic

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
-const database = require("./db/connection");
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
+const mongoSanitize = require("express-mongo-sanitize");
+
 const { postUser, postLogin } = require("./controllers/login.controller");
 const { mongoErrors, customErrors } = require("./errors/errors");
-const mongoSanitize = require("express-mongo-sanitize");
 const { getHello } = require("./controllers/getHello.controller");
 
 const app = express();
-database();
+const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI;
 
 app.use(mongoSanitize());
-
 app.use(cors());
 app.use(express.json());
 
 app.post("/api/register", postUser);
 app.post("/api/login", postLogin);
-app.get("api", getHello);
+app.get("/api", getHello);
 
 app.use((request, response) => {
   response.status(404).send({ msg: "Not found" });
@@ -25,4 +26,17 @@ app.use((request, response) => {
 app.use(mongoErrors);
 app.use(customErrors);
 
-module.exports = app;
+async function connectDB() {
+  try {
+    const conn = await mongoose.connect(MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    app.listen(PORT, () => {
+      console.log(`Listening on ${PORT}...`);
+    });
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  }
+}
+
+connectDB();
