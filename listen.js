82e76e0ca96197ca1a756e6cd705201ad6d67c15
annabenari <annabenari@me.com>
2023-09-22 14:29:42 +0100

    importing connection

--- listen.js
+++ listen.js
@@ -1,4 +1,6 @@
 const app = require("./app");
-const { PORT = 9090 } = process.env;
+const { PORT = 3000 } = process.env;
+const connection = require("./db/connection");
 
+connection();
 app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
